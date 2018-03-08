e73b834554fba5b273dd3fdbe2ff20ab9993c915
Izzy Moriguchi <izumimoriguchi@gmail.com>
2018-03-07 22:28:10 -0800

    Added popup window on main screen

--- app/src/main/java/edu/sjsu/izzymoriguchi/myapplication/MainActivity.java
+++ app/src/main/java/edu/sjsu/izzymoriguchi/myapplication/MainActivity.java
@@ -1,9 +1,17 @@
 package edu.sjsu.izzymoriguchi.myapplication;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     public void goToMealsActivity(View view) {
@@ -34,4 +43,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    public void showPopupInfo(View view) {
+        RelativeLayout layout = findViewById(R.id.relative_layout);
+        //instantiate the popup.xml layout file
+        LayoutInflater layoutInflater = (LayoutInflater) MainActivity.this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View customView = layoutInflater.inflate(R.layout.popup,null);
+
+        Button closePopupBtn = (Button) customView.findViewById(R.id.closePopupBtn);
+
+        //instantiate popup window
+        final PopupWindow popupWindow = new PopupWindow(customView, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        //display the popup window
+        popupWindow.showAtLocation(layout, Gravity.CENTER, 0, 0);
+
+        //close the popup window on button click
+        closePopupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                popupWindow.dismiss();
+            }
+        });
+    }
 }
