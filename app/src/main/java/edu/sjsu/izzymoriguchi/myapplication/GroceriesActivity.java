743558b612d1a6c619ad985c3779950ec964a5d5
Izzy Moriguchi <izumimoriguchi@gmail.com>
2018-03-11 10:45:29 -0700

    Implemented Swipe with add plus and minus button

--- app/src/main/java/edu/sjsu/izzymoriguchi/myapplication/GroceriesActivity.java
+++ app/src/main/java/edu/sjsu/izzymoriguchi/myapplication/GroceriesActivity.java
@@ -1,10 +1,17 @@
 package edu.sjsu.izzymoriguchi.myapplication;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import com.baoyz.swipemenulistview.SwipeMenu;
+import com.baoyz.swipemenulistview.SwipeMenuCreator;
+import com.baoyz.swipemenulistview.SwipeMenuItem;
+import com.baoyz.swipemenulistview.SwipeMenuListView;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -13,11 +20,13 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class GroceriesActivity extends AppCompatActivity {
+    private final String TAG = "GroceriesActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_groceries);
+        SwipeMenuListView listView = (SwipeMenuListView) findViewById(R.id.grocery_container_list_view);
 
 
 
@@ -59,9 +68,61 @@ public class GroceriesActivity extends AppCompatActivity {
                 }
             }
 
-            ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.activity_listview_portrait, arrOfGroceriesList);
-            ListView myListView = (ListView) findViewById(R.id.grocery_container_list_view);
-            myListView.setAdapter(adapter);
+            ArrayAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrOfGroceriesList);
+            listView.setAdapter(adapter);
+
+            SwipeMenuCreator creator = new SwipeMenuCreator() {
+
+                @Override
+                public void create(SwipeMenu menu) {
+                    // create "open" item
+                    SwipeMenuItem openItem = new SwipeMenuItem(getApplicationContext());
+                    // set item background
+                    openItem.setBackground(new ColorDrawable(Color.rgb(0xC9, 0xC9,
+                            0xCE)));
+                    // set item width
+                    openItem.setWidth(170);
+                    // set item title
+                    openItem.setTitle("Open");
+                    // set item title fontsize
+                    openItem.setTitleSize(18);
+                    // set item title font color
+                    openItem.setTitleColor(Color.WHITE);
+                    // add to menu
+                    menu.addMenuItem(openItem);
+
+                    // create "delete" item
+                    SwipeMenuItem deleteItem = new SwipeMenuItem(getApplicationContext());
+                    // set item background
+                    deleteItem.setBackground(new ColorDrawable(Color.rgb(0xF9,
+                            0x3F, 0x25)));
+                    // set item width
+                    deleteItem.setWidth(170);
+                    // set a icon
+                    deleteItem.setIcon(R.drawable.ic_plus);
+                    // add to menu
+                    menu.addMenuItem(deleteItem);
+                }
+            };
+            listView.setMenuCreator(creator);
+
+            listView.setOnMenuItemClickListener(new SwipeMenuListView.OnMenuItemClickListener() {
+                @Override
+                public boolean onMenuItemClick(int position, SwipeMenu menu, int index) {
+                    switch (index) {
+                        case 0:
+                            // open
+                            Log.d(TAG, "onMenuItemClick, and index: " + index);
+                            break;
+                        case 1:
+                            // delete
+                            Log.d(TAG, "onMenuItemClick, and index: " + index);
+                            break;
+                    }
+                    // false : close the menu; true : not close the menu
+                    return false;
+                }
+            });
 
         } catch (Exception ex) {
             ex.printStackTrace();
