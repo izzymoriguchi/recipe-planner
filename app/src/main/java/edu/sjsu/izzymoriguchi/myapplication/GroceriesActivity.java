94be45fe9112dce43ad45dac9aafb1611127824c
Izzy Moriguchi <izumimoriguchi@gmail.com>
2018-03-13 17:54:35 -0700

    Re-implemented the Grocery listview with custom Adapter and onTouchListener

--- app/src/main/java/edu/sjsu/izzymoriguchi/myapplication/GroceriesActivity.java
+++ app/src/main/java/edu/sjsu/izzymoriguchi/myapplication/GroceriesActivity.java
@@ -1,20 +1,13 @@
 package edu.sjsu.izzymoriguchi.myapplication;
 
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.ArrayAdapter;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
-
-import com.baoyz.swipemenulistview.SwipeMenu;
-import com.baoyz.swipemenulistview.SwipeMenuCreator;
-import com.baoyz.swipemenulistview.SwipeMenuItem;
-import com.baoyz.swipemenulistview.SwipeMenuListView;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -25,12 +18,15 @@ import java.util.HashSet;
 
 public class GroceriesActivity extends AppCompatActivity {
     private final String TAG = "GroceriesActivity";
-
+    private ArrayList<GroceriesModel> groceriesData;
+    private ArrayList<String> arrOfGroceriesList;
+    private MyCustomAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_groceries);
-        final SwipeMenuListView listView = (SwipeMenuListView) findViewById(R.id.grocery_container_list_view);
+//        final SwipeMenuListView listView = (SwipeMenuListView) findViewById(R.id.grocery_container_list_view);
+
 //        listView.add/
         FileInputStream ifile = null;
         ObjectInputStream in = null;
@@ -44,8 +40,9 @@ public class GroceriesActivity extends AppCompatActivity {
             ArrayList<NewDishModel> meals = lstOfMeals.getListOfMeals();
             HashMap<String, Integer> map = new HashMap<>();
             HashSet<String> set = new HashSet<>();
-            final ArrayList<String> arrOfGroceriesList = new ArrayList<>();
-            final ArrayList<GroceriesModel> groceriesData = new ArrayList<>();
+
+            arrOfGroceriesList = new ArrayList<>();
+            groceriesData = new ArrayList<>();
             for (int i = 0; i < meals.size(); i++) {
                 NewDishModel currModel = meals.get(i);
                 if (currModel.getSelectionCounter() > 0) {
@@ -69,59 +66,92 @@ public class GroceriesActivity extends AppCompatActivity {
                 }
             }
 
-
-            final ArrayAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrOfGroceriesList);
+            adapter = new MyCustomAdapter(arrOfGroceriesList, this);
+            final ListView listView = findViewById(R.id.grocery_container_list_view);
             listView.setAdapter(adapter);
 
-            SwipeMenuCreator creator = new SwipeMenuCreator() {
+            listView.setOnTouchListener(new OnSwipeTouchListener(GroceriesActivity.this) {
                 @Override
-                public void create(SwipeMenu menu) {
-                    // create "open" item
-                    SwipeMenuItem plusItem = new SwipeMenuItem(getApplicationContext());
-                    plusItem.setBackground(new ColorDrawable(Color.rgb(0xFF, 0xFF, 0xFF)));
-                    plusItem.setWidth(170);
-                    plusItem.setIcon(R.drawable.ic_plus);
-                    menu.addMenuItem(plusItem);
-
-                    // create "delete" item
-                    SwipeMenuItem minusItem = new SwipeMenuItem(getApplicationContext());
-                    minusItem.setBackground(new ColorDrawable(Color.rgb(0xFF, 0xFF, 0xFF)));
-                    minusItem.setWidth(170);
-                    minusItem.setIcon(R.drawable.ic_minus);
-                    menu.addMenuItem(minusItem);
+                public void onSwipeTop() {
+                    Toast.makeText(GroceriesActivity.this, "top", Toast.LENGTH_SHORT).show();
                 }
-            };
-            listView.setMenuCreator(creator);
-
-            listView.setOnMenuItemClickListener(new SwipeMenuListView.OnMenuItemClickListener() {
                 @Override
-                public boolean onMenuItemClick(int position, SwipeMenu menu, int index) {
-                    Log.d(TAG, "position index: " + position);
-                    GroceriesModel model = groceriesData.get(position);
-                    int i = arrOfGroceriesList.indexOf(model.toString());
-                    TextView item = (TextView)listView.getItemAtPosition(position);
+                public void onSwipeRight() {
+                    Toast.makeText(GroceriesActivity.this, "right", Toast.LENGTH_SHORT).show();
+                }
+                @Override
+                public void onSwipeLeft(MotionEvent e1) {
+                    // Whatever
+                    final int pos = listView.pointToPosition((int)e1.getX(), (int)e1.getY());
+                    View child = listView.getChildAt(pos);
+                    if (child != null){
+                        final ImageButton plus = (ImageButton) child.findViewById(R.id.add_btn);
+                        final ImageButton minus = (ImageButton) child.findViewById(R.id.delete_btn);
+                        if (plus != null) {
+                            if (plus.getVisibility() == View.INVISIBLE) {
+                                plus.setVisibility(View.VISIBLE);
+                            } else {
+                                plus.setVisibility(View.INVISIBLE);
+                            }
+                        }
+                        if (minus != null) {
+                            if (minus.getVisibility() == View.INVISIBLE) {
+                                minus.setVisibility(View.VISIBLE);
+                            } else {
+                                minus.setVisibility(View.INVISIBLE);
+                            }
+                        }
 
-                    item.setPaintFlags(item.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
-                    switch (index) {
-                        case 0:
-                            // increment
-                            model.incrementQty();
-                            arrOfGroceriesList.set(i, model.toString());
-                            break;
-                        case 1:
-                            // decrement
-                            model.decrementQty();
-                            arrOfGroceriesList.set(i, model.toString());
-                            break;
+                        plus.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                incrementCount(pos);
+                                plus.setVisibility(View.INVISIBLE);
+                                minus.setVisibility(View.INVISIBLE);
+                            }
+                        });
+
+                        minus.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                decrementCount(pos);
+                                plus.setVisibility(View.INVISIBLE);
+                                minus.setVisibility(View.INVISIBLE);
+                            }
+                        });
+
                     }
-                    adapter.notifyDataSetChanged();
-                    // false : close the menu; true : not close the menu
-                    return false;
+                    Toast.makeText(GroceriesActivity.this, "left", Toast.LENGTH_SHORT).show();
                 }
+                @Override
+                public void onSwipeBottom() {
+                    Toast.makeText(GroceriesActivity.this, "bottom", Toast.LENGTH_SHORT).show();
+                }
+
             });
+
 
         } catch (Exception ex) {
             ex.printStackTrace();
         }
     }
+
+    public void incrementCount(int pos) {
+        GroceriesModel model = groceriesData.get(pos);
+        final int i = arrOfGroceriesList.indexOf(model.toString());
+        model.incrementQty();
+        arrOfGroceriesList.set(i, model.toString());
+        adapter.notifyDataSetChanged();
+    }
+
+    public void decrementCount(int pos) {
+        GroceriesModel model = groceriesData.get(pos);
+        final int i = arrOfGroceriesList.indexOf(model.toString());
+        model.decrementQty();
+        arrOfGroceriesList.set(i, model.toString());
+        adapter.notifyDataSetChanged();
+    }
+
+
+
 }
